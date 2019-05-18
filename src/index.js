10a09067165737b69e06ffe169583b9b929df741
MatthewK0617 <41757629+MatthewK0617@users.noreply.github.com>
2019-05-17 20:18:55 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import { db } from "./utils/firebase";
+
+function App() {
+  let [todos, setTodos] = React.useState([]);
+  let [newTxt, setNewTxt] = React.useState("");
+
+  // load from DB and populate "todos"
+
+  React.useEffect(() => {
+    async function loadTodos() {
+      let data = await db.collection("todos").get();
+      setTodos(data.docs);
+    }
+    loadTodos();
+  }, []);
+
+  let deleteTodo = id => {
+    db.collection("todos")
+      .doc(id)
+      .delete();
+    let newTodos = todos.filter(v => v.id !== id);
+    setTodos(newTodos);
+  };
+
+  let addTodo = () => {
+    let obj = {
+      txt: "hi",
+      author: "Mehtewgeem"
+    };
+    let docref = db.collection("todos").add(obj); // docref uses id to REFER to the document
+    let doc = db
+      .collection("todos")
+      .doc(docref.id)
+      .get();
+    setTodos([...todos, doc]);
+  };
+
+  function onSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      {todos.map(v => (
+        <button
+          key={v.id}
+          onClick={() => {
+            deleteTodo(v.id);
+          }}
+        >
+          {v.data().txt} - {v.data().author}
+        </button>
+      ))}
+      <hr />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={newTxt}
+          onChange={event => setNewTxt(event.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
